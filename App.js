12890294657b038d2f36c450d6e10d0a57f5b6ac
leonidvaidner <leonid.vaydner@yandex.ru>
2022-11-15 07:55:30 +0400

    delete item

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, SafeAreaView, Button, Alert, Image, FlatList} from 'react-native';
+import Header from "./components/header";
+import {useState} from "react";
+import ListItem from "./components/listItem";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [listOfItem, setListOfItems] = useState([
+        {
+            text: 'Buy watch', index: 1
+        },
+        {
+            text: 'love Evgenia', index: 2
+        },
+        {
+            text: 'Buy tesla', index: 3
+        },
+        {
+            text: 'Buy house', index: 4
+        }
+    ])
+
+    function deleteItem(index){
+       let newArray = listOfItem.filter((element)=> element.index !== index)
+        setListOfItems(newArray)
+    }
+
+    return (
+        <View>
+            <Header/>
+            <FlatList data={listOfItem} renderItem={({item})=> (
+                <ListItem el={item} deleteFunction={deleteItem}/>
+            )}/>
+            <StatusBar/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/header.js
@@ -0,0 +1,25 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+
+export default function Header() {
+
+
+    return (
+        <View style={styles.main}>
+            <Text style={styles.text}>Список дел</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    main: {
+        paddingTop: 50,
+        height: 100,
+        backgroundColor: 'silver'
+    },
+    text: {
+        fontSize: 20,
+        color: 'red',
+        fontStyle: 'bold',
+        textAlign: 'center'
+    }
+});
